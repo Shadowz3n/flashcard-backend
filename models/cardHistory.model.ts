29bd6c752939ac5c71bd9daaf3ddc55110951c44
Gerson Samuel Fuchs <fuchsgerson@gmail.com>
2023-06-11 15:29:25 -0300

    feat(cardHistory.model.ts): created model data

---
+++ models/cardHistory.model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+export interface ICardHistory extends Document {
+  userId: string;
+  cardId: string;
+  date: Date;
+  direction: string;
+}
+
+export const cardHistorySchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: [true, "User ID field is required."],
+  },
+  cardId: {
+    type: String,
+    required: [true, "Card ID field is required."],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  direction: {
+    type: String,
+    required: [true, "Direction field is required."],
+  },
+});
+
+export const CardHistory = mongoose.model<ICardHistory>(
+  "CardHistory",
+  cardHistorySchema
+);
