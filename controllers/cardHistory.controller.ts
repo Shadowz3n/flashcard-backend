113b16793a394e7bc8fcef1810b5de340557c1b3
Gerson Samuel Fuchs <fuchsgerson@gmail.com>
2023-06-14 23:28:53 -0300

    wip

--- controllers/cardHistory.controller.ts
+++ controllers/cardHistory.controller.ts
@@ -11,7 +11,6 @@ export const updateCardHistory = async (
     const userId = req.user?.id;
     const cardId = req.params.id;
     const { direction } = req.body;
-
     if (direction !== "left" && direction !== "right") {
       res.status(400).json({ error: "Invalid direction value" });
       return;
@@ -64,6 +63,106 @@ export const getCardsByDeckIdWithHistory = async (
     });
 
     res.status(200).json({ ...deck, cards: cardsWithHistory });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+};
+
+export const getAllDecksWithHistory = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const userId = req.user?.id;
+    const decks = await Deck.find().lean().exec();
+    const cardIds = decks.map((deck) => deck.cards).flat();
+    const cards = await Card.find({ _id: { $in: cardIds } }).exec();
+
+    const cardHistoryPromises = cardIds.map((cardId) =>
+      CardHistory.find({ userId, cardId }).exec()
+    );
+    const cardHistories = await Promise.all(cardHistoryPromises);
+
+    const cardsWithHistory = cards.map((card, index) => {
+      const history = cardHistories[index].map((ch) => ({
+        date: ch.date,
+        direction: ch.direction,
+      }));
+
+      return {
+        ...card.toObject(),
+        history: history,
+      };
+    });
+
+    const decksWithHistory = decks.map((deck, index) => ({
+      ...deck,
+      cards: cardsWithHistory.filter((card) =>
+        deck.cards.includes(card._id.toString())
+      ),
+    }));
+    res.status(200).json(decksWithHistory);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+};
+
+export const getRecentlyPlayedDecks = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const userId = req.user?.id;
+    const decks = await Deck.find().lean().exec();
+
+    const cardIds = decks.map((deck) => deck.cards).flat();
+    const cards = await Card.find({ _id: { $in: cardIds } }).exec();
+
+    const cardHistoryPromises = cardIds.map((cardId) =>
+      CardHistory.find({ userId, cardId }).exec()
+    );
+    const cardHistories = await Promise.all(cardHistoryPromises);
+
+    const cardsWithHistory = cards.map((card, index) => {
+      const history = cardHistories[index].map((ch) => ({
+        date: ch.date,
+        direction: ch.direction,
+      }));
+
+      return {
+        ...card.toObject(),
+        history: history,
+      };
+    });
+
+    const decksWithHistory = decks.map((deck, index) => ({
+      ...deck,
+      cards: cardsWithHistory.filter((card) =>
+        deck.cards.includes(card._id.toString())
+      ),
+    }));
+
+    const sortedDecks = decksWithHistory
+      .filter((deck) => deck.cards.some((card) => card.history.length > 0))
+      .sort((a, b) => {
+        const lastPlayedA = a.cards.reduce((latestDate, card) => {
+          const lastHistoryEntry = card.history[card.history.length - 1];
+          const cardDate = new Date(lastHistoryEntry.date);
+          return cardDate > latestDate ? cardDate : latestDate;
+        }, new Date(0));
+
+        const lastPlayedB = b.cards.reduce((latestDate, card) => {
+          const lastHistoryEntry = card.history[card.history.length - 1];
+          const cardDate = new Date(lastHistoryEntry.date);
+          return cardDate > latestDate ? cardDate : latestDate;
+        }, new Date(0));
+
+        return lastPlayedB.getTime() - lastPlayedA.getTime();
+      });
+
+    const recentlyPlayedDecks = sortedDecks.slice(0, 3);
+
+    res.status(200).json(recentlyPlayedDecks);
   } catch (error) {
     res.status(400).json({ error: error });
   }
